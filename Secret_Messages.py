276059c0ac076843882a58945ebbf4d0940348d5
Thomas Ransom <32624861+trransom@users.noreply.github.com>
2018-05-12 11:01:15 -0400

    Imported OrderedDict to create ordered dictionaries

--- Secret_Messages.py
+++ Secret_Messages.py
@@ -1,9 +1,12 @@
 #Secret Messages
 from ciphers import Cipher
+from atbash_cipher import Atbash
 
 print("This is a Secret Messages program. Choose your cipher and encrypt or decrypt your message!\n\n")
 print("These are the current ciphers available:\n\n-Atbash\n-Hill\n-Polybius\n\n")
 input("Which cipher would you like to use?\n")
+t = Atbash()
+print(t.backwards)
 
-class crpytography(Cipher):
+#class crpytography(Cipher):
 
--- atbash_cipher.py
+++ atbash_cipher.py
@@ -1,1 +1,21 @@
 # Atbash Cipher
+from ciphers import Cipher
+from collections import OrderedDict
+
+class Atbash(Cipher):
+#    backwards = {number: letter for letter, number in zip('abcdefghijklmnopqrstuvwxyz'[::-1], range(1, 27))}
+#	forwards = {number: letter for letter, number in zip('abcdefghijklmnopqrstuvwxyz', range(1, 27))}
+	forwards = OrderedDict(zip(string.ascii_lowercase, string.ascii_lowercase[::-1]))
+	backwards = OrderedDict(zip(string.ascii_lowercase[::-1], range(1, 27)))
+
+    def __init__(self, string):
+        pass
+
+    def encrypt(self, string):
+		ans = ''
+        for char in string:
+			int = backwards[char]
+			
+
+    def decrypt(self, text):
+        pass

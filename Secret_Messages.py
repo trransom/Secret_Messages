57d0104324028de9575764fffa6a8b7a2d804752
Thomas Ransom <32624861+trransom@users.noreply.github.com>
2018-05-14 13:15:55 -0400

    Finished encrypt and decrypt methods on polybius cipher

--- Secret_Messages.py
+++ Secret_Messages.py
@@ -7,7 +7,10 @@ print("This is a Secret Messages program. Choose your cipher and encrypt or decr
 print("These are the current ciphers available:\n\n-Atbash\n-Hill\n-Polybius\n\n")
 input("Which cipher would you like to use?\n")
 pb = Polybius()
-print(pb.poly_dict)
+ans1 = pb.encrypt('BAT')
+ans2 = pb.decrypt(ans1)
+print(ans1 + '\n')
+print(ans2)
 
 #class crpytography(Cipher):
 
--- polybius_cipher.py
+++ polybius_cipher.py
@@ -3,14 +3,39 @@ from ciphers import Cipher
 from collections import OrderedDict
 import string
 
+poly_dict = {'a': 11, 'b': 12, 'c': 13, 'd': 14, 'e': 15, 
+				'f': 21, 'g': 22, 'h': 23, 'i': 24, 'j': 24, 'k': 25, 
+				'l': 31, 'm': 32, 'n': 33, 'o': 34, 'p': 35, 
+				'q': 41, 'r': 42, 's': 43, 't': 44, 'u': 45,
+				'v': 51, 'w': 52, 'x': 53, 'y': 54, 'z': 55}
+				
+				
+r_poly_dict = {'11': 'a', '12': 'b', '13': 'c', '14': 'd', '15': 'e', 
+				'21': 'f', '22': 'g', '23': 'h', '24': '(i\j)', '25': 'k',
+				'31': 'l', '32': 'm', '33': 'n', '34': 'o', '35': 'p', 
+				'41': 'q', '42': 'r', '43': 's', '44': 't', '45': 'u', 
+				'51': 'v', '52': 'w', '53': 'x', '54': 'y', '55': 'z'}
+				
+				
 class Polybius(Cipher):
-	poly_dict = {'a':(1, 1), 'b':(1, 2)}
+	num_list = [11, 12, 13, 14, 15, 21, 22, 23, 24, 25, 31, 32, 33, 34, 35, 41, 42, 43, 44, 45, 51, 52, 53, 54, 55]
 	
 	def __init__(self):
 		pass
+		
+	def encrypt(self, string):
+		ans = ''
+		s = string.lower()
+		for char in s:
+			new_char = poly_dict[char]
+			ans += str(new_char) + ' '
+		ans = ans[:-1]
+		return ans
 	
-	def encrypt(self):
-		pass
-	
-	def decrypt(self):
-		pass
+	def decrypt(self, string):
+		ans = ''
+		code = string.split(' ')
+		for num in code:
+			new_num = r_poly_dict[num]
+			ans += str(new_num)
+		return ans

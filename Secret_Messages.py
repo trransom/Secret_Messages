a888e166e15c3872185c22aeb37992885dd3e2d6
Thomas Ransom <32624861+trransom@users.noreply.github.com>
2018-05-14 14:20:25 -0400

    Deleted Hill file, replaced with keyword

--- Secret_Messages.py
+++ Secret_Messages.py
@@ -2,15 +2,14 @@
 from ciphers import Cipher
 from atbash_cipher import Atbash
 from polybius_cipher import Polybius
+from hill_cipher import Hill
 
 print("This is a Secret Messages program. Choose your cipher and encrypt or decrypt your message!\n\n")
 print("These are the current ciphers available:\n\n-Atbash\n-Hill\n-Polybius\n\n")
 input("Which cipher would you like to use?\n")
-pb = Polybius()
-ans1 = pb.encrypt('BAT')
-ans2 = pb.decrypt(ans1)
-print(ans1 + '\n')
-print(ans2)
+
+hl = Hill()
+print(hl.x)
 
 #class crpytography(Cipher):
 

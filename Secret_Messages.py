f2f42426b2b69981ce754d3820f4a5962d5f0eb9
Thomas Ransom <32624861+trransom@users.noreply.github.com>
2018-05-15 13:34:39 -0400

    Updated keyword cipher's decrypt method by first clearing each dictionary and list

--- Secret_Messages.py
+++ Secret_Messages.py
@@ -2,14 +2,15 @@
 from ciphers import Cipher
 from atbash_cipher import Atbash
 from polybius_cipher import Polybius
-from hill_cipher import Hill
+from keyword_cipher import Keyword
 
 print("This is a Secret Messages program. Choose your cipher and encrypt or decrypt your message!\n\n")
 print("These are the current ciphers available:\n\n-Atbash\n-Hill\n-Polybius\n\n")
-input("Which cipher would you like to use?\n")
+code = input("Which cipher would you like to use?\n").toLower()
 
-hl = Hill()
-print(hl.x)
+key = Keyword()
+print(key.encrypt('kryptos', 'knowledge'))
+print(key.decrypt('kryptos', 'dghvetpst'))
 
 #class crpytography(Cipher):
 
--- keyword_cipher.py
+++ keyword_cipher.py
@@ -4,27 +4,51 @@ import string
 
 alphabet = string.ascii_lowercase
 key_list = []
-alph_dict = {}
+encrypt_dict = {}
+decrypt_dict = {}
 
+def clearDict(dictionary):
+	dictionary.clear()
+
+#Make constructor handle overridden keyword, define 
+#your own keyword.
 class Keyword(Cipher):
 	
 	def __init__(self):
 		pass
 		
 	def encrypt(self, keyword, string):
+		key_list.clear()
+		clearDict(encrypt_dict)
 		key_list[1:1] = list(keyword)
 		for char in alphabet:
 			if char not in key_list:
 				key_list.append(char)
 		i = 0
 		for item in key_list:
-			alph_dict[alphabet[i]] = item
+			encrypt_dict[alphabet[i]] = item
 			i += 1
 		ans = ''
 		for letter in string:
-			new_letter = alph_dict[letter]
+			new_letter = encrypt_dict[letter]
 			ans += new_letter
 		return ans
 		
 	def decrypt(self, keyword, string):
-		pass
+		key_list.clear()
+		clearDict(decrypt_dict)
+		key_list[1:1] = list(keyword)
+		for char in alphabet:
+			if char not in key_list:
+				key_list.append(char)
+		i = 0
+		for item in key_list:
+			if i <= 25:
+				decrypt_dict[key_list[i]] = alphabet[i]
+				i += 1
+		ans = ''
+		for letter in string:
+			new_letter = decrypt_dict[letter]
+			ans += new_letter
+		return ans
+		

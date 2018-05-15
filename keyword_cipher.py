e1f56386ed26031f328395f978e6c9968935a6ab
Thomas Ransom <32624861+trransom@users.noreply.github.com>
2018-05-15 08:15:40 -0400

    Defined the encrypt method for the keyword cipher

--- keyword_cipher.py
+++ keyword_cipher.py
@@ -1,1 +1,30 @@
 # Keyword cipher
+from ciphers import Cipher
+import string
+
+alphabet = string.ascii_lowercase
+key_list = []
+alph_dict = {}
+
+class Keyword(Cipher):
+	
+	def __init__(self):
+		pass
+		
+	def encrypt(self, keyword, string):
+		key_list[1:1] = list(keyword)
+		for char in alphabet:
+			if char not in key_list:
+				key_list.append(char)
+		i = 0
+		for item in key_list:
+			alph_dict[alphabet[i]] = item
+			i += 1
+		ans = ''
+		for letter in string:
+			new_letter = alph_dict[letter]
+			ans += new_letter
+		return ans
+		
+	def decrypt(self, keyword, string):
+		pass

5320fa4b3b17376b4745e4205fc0d9b54dc6b43f
Thomas Ransom <32624861+trransom@users.noreply.github.com>
2018-05-18 15:03:59 -0400

    Switched out for loops for comprehensions in encrypt method

--- keyword_cipher.py
+++ keyword_cipher.py
@@ -7,8 +7,10 @@ key_list = []
 encrypt_dict = {}
 decrypt_dict = {}
 
-def clearDict(dictionary):
+#Empties the dictionary given as an argument
+def clear(dictionary, list):
 	dictionary.clear()
+	list.clear()
 
 #Make constructor handle overridden keyword, define 
 #your own keyword.
@@ -18,25 +20,26 @@ class Keyword(Cipher):
 		pass
 		
 	def encrypt(self, keyword, string):
-		key_list.clear()
-		clearDict(encrypt_dict)
+		#clear the encryption dictionary and the keyword list
+		clear(encrypt_dict, key_list)
+		
+		#Add the new keyword to the key list
 		key_list[1:1] = list(keyword)
-		for char in alphabet:
-			if char not in key_list:
-				key_list.append(char)
-		i = 0
-		for item in key_list:
-			encrypt_dict[alphabet[i]] = item
-			i += 1
-		ans = ''
-		for letter in string:
-			new_letter = encrypt_dict[letter]
-			ans += new_letter
+		
+		#Create the rest of the alphabet, and append it to the keylist.
+		alph = [char for char in alphabet if char not in key_list]
+		key_list.extend(alph)
+		
+		#Create the encryption dictionary
+		encrypt_dict.update({alph_letter: code_letter for alph_letter, code_letter in zip(alphabet, key_list)})
+		
+		#Create the answer string
+		ans_string = [encrypt_dict[char] for char in string]
+		ans = ''.join(ans_string)
 		return ans
 		
 	def decrypt(self, keyword, string):
-		key_list.clear()
-		clearDict(decrypt_dict)
+		clear(decrypt_dict, key_list)
 		key_list[1:1] = list(keyword)
 		for char in alphabet:
 			if char not in key_list:

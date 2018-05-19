4b9ceca2b0bbd526e61e2dfaab6adc2814f9ada1
Thomas Ransom <32624861+trransom@users.noreply.github.com>
2018-05-19 09:10:35 -0400

    PEP 8 tab change

--- polybius_cipher.py
+++ polybius_cipher.py
@@ -18,7 +18,11 @@ r_poly_dict = {'11': 'a', '12': 'b', '13': 'c', '14': 'd', '15': 'e',
 				
 				
 class Polybius(Cipher):
-	num_list = [11, 12, 13, 14, 15, 21, 22, 23, 24, 25, 31, 32, 33, 34, 35, 41, 42, 43, 44, 45, 51, 52, 53, 54, 55]
+	num_list = [11, 12, 13, 14, 15, 
+				21, 22, 23, 24, 25,
+				31, 32, 33, 34, 35, 
+				41, 42, 43, 44, 45, 
+				51, 52, 53, 54, 55]
 		
 	'''
 		Encrypts the given string by turning it to lowercase and then running it against

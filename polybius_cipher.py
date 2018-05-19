4e7c43c6816b0f01e7973528a6959c824883a7cc
Thomas Ransom <32624861+trransom@users.noreply.github.com>
2018-05-19 07:23:49 -0400

    Deleted the init method from the polybius class

--- polybius_cipher.py
+++ polybius_cipher.py
@@ -19,9 +19,6 @@ r_poly_dict = {'11': 'a', '12': 'b', '13': 'c', '14': 'd', '15': 'e',
 				
 class Polybius(Cipher):
 	num_list = [11, 12, 13, 14, 15, 21, 22, 23, 24, 25, 31, 32, 33, 34, 35, 41, 42, 43, 44, 45, 51, 52, 53, 54, 55]
-	
-	def __init__(self):
-		pass
 		
 	def encrypt(self, string):
 		ans = ''

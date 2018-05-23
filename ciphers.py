fda25ba1d24ab56d8eaec8d1c8eb0dba0b9ebc15
Thomas Ransom <32624861+trransom@users.noreply.github.com>
2018-05-23 13:06:30 -0400

    Refactored the ciphers class

--- ciphers.py
+++ ciphers.py
@@ -1,9 +1,28 @@
 # Ciphers
+from collections import OrderedDict
+import string
 
-class Cipher():
-	def encrypt(self):
-		raise NotImplementedError
+class Cipher
+
+	alphabet = string.ascii_lowercase
+	crypt_alphabet = 'enhgtwmbcdsojlfkzqyruviaxp'
+	encrypt_dict = OrderedDict(zip(alphabet, crypt_alphabet))
+	decrypt_dict = OrderedDict(zip(crypt_alphabet, alphabet))
+	
+	def __init__(self, crypt_alphabet):
+		self.message = message
+		self.crypt_alphabet = crypt_alphabet
 		
-	def decrypt(self, string):
-		raise NotImplementedError
+		
+	def encrypt(self, message):
+		ans = ''
+		for char in message:
+			ans += encrypt_dict[char]
+		return ans
+		
+	def decrypt(self, message):
+		ans = ''
+		for char in message:
+			ans += decrypt_dict[char]
+		return ans
 		
